2d4f85bdc0fbc98519a4668a4c1646f14d69ede1
anderson365 <anderson1991rs@gmail.com>
2022-04-01 20:52:31 -0300

    added Board component

---
+++ components/Board.jsx
@@ -0,0 +1,15 @@
+import Row from "../components/Row"
+
+export default function Board(){
+
+    return <>
+        <Row></Row>
+        <Row even></Row>
+        <Row></Row>
+        <Row even></Row>
+        <Row></Row>
+        <Row even></Row>
+        <Row></Row>
+        <Row even></Row>
+    </>
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,12 @@
-import styles from "../styles/boardPage.module.css"
-import Row from "../components/Row"
+import styles from "../styles/index.module.css"
+import Board from "../components/Board"
 
 export default function chess_board(){
 
-    return <div className={styles.background}>
-        <h1 className={styles.title}>Chess Board with Next.js</h1>
-        <Row></Row>
-        <Row even></Row>
-        <Row></Row>
-        <Row even></Row>
-        <Row></Row>
-        <Row even></Row>
-        <Row></Row>
-        <Row even></Row>
-    
-    </div>
+    return <>
+        <div className={styles.background}>
+            <h1 className={styles.title}>Chess Board with Next.js</h1>
+            <Board></Board>
+        </div>
+    </>
 }
